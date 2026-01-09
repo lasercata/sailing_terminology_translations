3e1daebcbecb0fba6795a5124481062ace0ef786
lasercata <67599917+lasercata@users.noreply.github.com>
2026-01-09 16:35:52 +0100

    count script now counts missing translations

--- count_terms_count.sh
+++ count_terms_count.sh
@@ -5,4 +5,8 @@
 
 nb_terms=$(grep Sailing_voc.tex -v -e Header -e subsection -e "     % {{{2" | grep -e {{{2 -e {{{3 | wc -l)
 
-echo Number of terms: $nb_terms
+nb_not_translated=$(grep Sailing_voc.tex -v -e Header -e subsection -e "     % {{{2" | grep -e {{{2 -e {{{3 -A2 | grep -v -e "& " -e "--" -e "{{{" | wc -l)
+
+echo "Number of terms: $nb_terms"
+
+echo "Number of non translated slots: $nb_not_translated"
